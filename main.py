b6fe84f7f54b4316542d306a641553095c42fc89
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-23 18:14:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,22 @@ st.bar_chart(chart_data)
 
 title = st.text_input('Enter User Name', 'Life of Brian')
 number = st.number_input('Insert a User Age')
-d = st.date_input(
-     "When's your birthday",
-     datetime.date(2019, 7, 6))
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
+
+
 result =  st.button("Click Submit")
 if result:
      st.write('User Is :', title)
      st.write('Age :' ,number)
-     st.write('DOB :' ,d)
+   
      
 else:
      st.write("No Click")
