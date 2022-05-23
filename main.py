28669a27656f0b6e8e5410af96bdef5d0e4cfa10
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-23 16:44:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import numpy as np
 import altair as alt
 
 df = pd.DataFrame(
-     np.random.randn(100, 3),
-     columns=['a', 'b', 'c'])
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
 
-c = alt.Chart(df).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-
-st.write(c)
-
+st.dataframe(df)  # Same as st.write(df)
