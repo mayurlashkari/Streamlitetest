d1949c87cc6f892d73f7eed516310f7faef39775
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-23 18:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import cv2
+import numpy as np
+
+
 
 chart_data = pd.DataFrame(
      np.random.randn(10, 3),
@@ -16,11 +20,16 @@ number = st.number_input('Insert a User Age')
 img_file_buffer = st.camera_input("Take a picture")
 
 if img_file_buffer is not None:
-    # To read image file buffer as bytes:
     bytes_data = img_file_buffer.getvalue()
-    # Check the type of bytes_data:
-    # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(cv2_img))
+
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
 
 
 result =  st.button("Click Submit")
