4bcac9ad785d5b365392bd59898720a0fcc9f625
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-23 16:45:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ df = pd.DataFrame(
     np.random.randn(50, 20),
     columns=('col %d' % i for i in range(20)))
 
-st.dataframe(df)  # Same as st.write(df)
+#st.dataframe(df)  # Same as st.write(df)
+st.dataframe(df.style.highlight_max(axis=0))
