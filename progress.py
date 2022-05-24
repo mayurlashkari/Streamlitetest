94e5801f58ee159a6fd602e69a16b93a28641add
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-24 11:50:42 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -3,9 +3,10 @@ import streamlit as st
 my_bar = st.progress(0)
 
 for percent_complete in range(100):
-     time.sleep(0.1)
+     time.sleep(0.01)
      my_bar.progress(percent_complete + 1)
 
 with st.spinner('Wait for it...'):
     time.sleep(5)
-st.error('This is an error')
+e = RuntimeError('This is an exception of type RuntimeError')
+st.exception(e)
