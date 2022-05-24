544170eb4e052706cfae6f31eb1717ec99874629
lashkari.mayur@gmail.com <lashkari.mayur@gmail.com>
2022-05-24 11:44:47 +0530

    Progress Bar

--- progress.py
+++ progress.py
@@ -5,3 +5,7 @@ my_bar = st.progress(0)
 for percent_complete in range(100):
      time.sleep(0.1)
      my_bar.progress(percent_complete + 1)
+
+with st.spinner('Wait for it...'):
+    time.sleep(5)
+st.success('Done!')
