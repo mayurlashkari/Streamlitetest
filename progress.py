9827aec4190ae112c0a9f81b1081dbcc7717c519
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-24 11:53:38 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -2,8 +2,21 @@ import time
 import streamlit as st
 
 
-name = st.text_input('Name')
-if not name:
-  st.warning('Please input a name.')
-  st.stop()
-st.success('Thank you for inputting a name.')
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
+
+form = st.form("my_form")
+form.slider("Inside the form")
+st.slider("Outside the form")
+
+# Now add a submit button to the form:
+form.form_submit_button("Submit")
