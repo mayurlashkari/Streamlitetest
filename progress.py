4f7562cd7e24944768c1ffd872531cd2af440ad6
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-24 11:51:45 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -1,12 +1,9 @@
 import time
 import streamlit as st
-my_bar = st.progress(0)
 
-for percent_complete in range(100):
-     time.sleep(0.01)
-     my_bar.progress(percent_complete + 1)
 
-with st.spinner('Wait for it...'):
-    time.sleep(5)
-e = RuntimeError('This is an exception of type RuntimeError')
-st.exception(e)
+name = st.text_input('Name')
+if not name:
+  st.warning('Please input a name.')
+  st.stop()
+st.success('Thank you for inputting a name.')
