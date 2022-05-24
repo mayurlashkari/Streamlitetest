ed19b0e541398601ac36dc7fe8dd18591d7efba7
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-24 12:59:18 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -36,22 +36,5 @@ df1 = pd.DataFrame(
 
 my_table = st.table(df1)
 
-df2 = pd.DataFrame(
-    np.random.randn(50, 20),
-    columns=('col %d' % i for i in range(20)))
 
 
-# Assuming df1 and df2 from the example above still exist...
-my_chart = st.line_chart(df1)
-my_chart.add_rows(df2)
-# Now the chart shown in the Streamlit app contains the data for
-# df1 followed by the data for df2.
-my_chart = st.vega_lite_chart({
-     'mark': 'line',
-     'encoding': {'x': 'a', 'y': 'b'},
-     'datasets': {
-       'some_fancy_name': df1,  # <-- named dataset
-      },
-     'data': {'name': 'some_fancy_name'},
- }),
-my_chart.add_rows(some_fancy_name=df2)  # <-- name used as keyword
