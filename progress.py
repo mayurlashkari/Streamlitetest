3a10be5204f54b295fc55f421f6b9934849204cb
Mayur Lashkari <lashkari.mayur@gmail.com>
2022-05-24 12:21:56 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -40,4 +40,9 @@ df2 = pd.DataFrame(
     np.random.randn(50, 20),
     columns=('col %d' % i for i in range(20)))
 
-my_table.add_rows(df2)
+
+# Assuming df1 and df2 from the example above still exist...
+my_chart = st.line_chart(df1)
+my_chart.add_rows(df2)
+# Now the chart shown in the Streamlit app contains the data for
+# df1 followed by the data for df2.
